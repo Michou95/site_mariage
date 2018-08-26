037f701174d4295222be5620fcd14a057c4a59fb
Michou95 <regent.mickael.mai@gmail.com>
2018-08-26 22:46:35 +0200

    modif requete de selection photo par categorie

--- image_par_categorie.php
+++ image_par_categorie.php
@@ -9,7 +9,7 @@ $categorie = strip_tags(trim($_POST['categorie']));
 function getPhotoByCategory(string $category):array{
   $connexion = getDB();
   if($category == "mairie" || $category == "vin_honneur" || $category == "salle"){
-    $sql = "SELECT url FROM photos WHERE categorie = ?;"; 
+    $sql = "SELECT id_photo, url FROM photos WHERE categorie = ?;"; 
     $query = $connexion->prepare($sql);
     $query->execute(array($category));
     $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
