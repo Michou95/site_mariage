8db29f4470f358a2685084e5ae27d4e7e63fb98b
kevin regent <lemissieu-du-95@hotmail.fr>
2018-08-26 15:36:49 +0200

    dispatch des head body footer

--- indexe.php
+++ indexe.php
@@ -1,41 +1,29 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Notre Mariage</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css"/>
-  </head>
-  <body>
-    <!-- <audio autoplay>
-      <source src="son.mp3">
-    </audio> -->
-    <header>
-      <div class="title">
-        <h1>Mickael & Jennifer</h1>
-        <h2>28 Juillet 2018</h2>
-      </div>
-    </header>
-    
-    <form action="test.html" method="post">
-      <div class="form_row">
-          <label for="name">Votre nom</label>
-            <input class="name" type="text" name="name" value="">
-          <input class="submit" type="submit" name="submit" value="Envoyer">
-      </div>
-    </form>
+  <?php include_once'head.php' ?>
 
-    <a href="index.php?accubens">Photos Charline</a>
-    <a href="index.php">Photos de tout le monde</a>
+  <!-- <audio autoplay>
+    <source src="son.mp3">
+  </audio> -->
 
-      <div class="container">
-        <!-- CONTENUE AFFICHAGE SITE -->
-      </div>
+  <header>
+    <div class="title">
+      <h1>Mickael & Jennifer</h1>
+      <h2>28 Juillet 2018</h2>
+    </div>
+  </header>
 
-    </body>
-    <footer>
-      &copy; Mickael Régent tout droit réservé
-    </footer>
-  </body>
-</html>
+  <form action="test.html" method="post">
+    <div class="form_row">
+        <label for="name">Votre nom</label>
+          <input class="name" type="text" name="name" value="">
+        <input class="submit" type="submit" name="submit" value="Envoyer">
+    </div>
+  </form>
+
+  <a href="index.php?accubens">Photos Charline</a>
+  <a href="index.php">Photos de tout le monde</a>
+
+  <div class="container">
+    <!-- CONTENUE AFFICHAGE SITE -->
+  </div>
+
+<?php include_once'footer.php' ?>
