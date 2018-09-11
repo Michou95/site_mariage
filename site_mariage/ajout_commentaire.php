7b91ad5151b1610e7c0f64486023bcd5841e0ff7
Kévin Cessac <cessac.kevin@gmail.com>
2018-09-11 16:54:29 +0200

    probleme apostrophe résolu
    ...

--- site_mariage/ajout_commentaire.php
+++ site_mariage/ajout_commentaire.php
@@ -5,9 +5,9 @@ if($_POST) {
     require_once ('fonctions.php');
     $connexion = getDB();
 
-    $idPhoto = htmlSpecialChars(strip_tags($_POST['id_photo']));
-    $username = htmlSpecialChars(strip_tags($_POST['name']));
-    $content = htmlSpecialChars(strip_tags($_POST['commentary']));
+    $idPhoto = htmlSpecialChars(addslashes(strip_tags($_POST['id_photo'])));
+    $username = htmlSpecialChars(addslashes(strip_tags($_POST['name'])));
+    $content = htmlSpecialChars(addslashes(strip_tags($_POST['commentary'])));
     $timestamp = time();
 
     $sql = "INSERT INTO commentary (photo_id, username, content, timestamp) VALUES ('$idPhoto', '$username', '$content', '$timestamp')";
